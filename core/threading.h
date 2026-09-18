b0e12324609a8910be8bc769c591264a5ba4b413
Joshua Antikainen <joshua.antiakainen@gmail.com>
2026-09-18 07:13:45 -0600

    formatting

--- core/threading.h
+++ core/threading.h
@@ -7,16 +7,16 @@
 namespace core::threads
 {
 	using task_fn = rawptr (*)(rawptr arg);
-	using thread = thrd_t;
-	using mutex = mtx_t;
+	using thread  = thrd_t;
+	using mutex   = mtx_t;
 
 	enum class result
 	{
-		success  = thrd_success,
+		success   = thrd_success,
 		no_memory = thrd_nomem,
 		timed_out = thrd_timedout,
-		busy     = thrd_busy,
-		error    = thrd_error
+		busy      = thrd_busy,
+		error     = thrd_error
 	};
 }
 #endif
