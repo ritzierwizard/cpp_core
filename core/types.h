b1903568bb81f0c826255c6f8a05c1283757cd3b
Joshua Antikainen <joshua.antiakainen@gmail.com>
2026-09-18 22:58:53 -0600

    update

--- core/types.h
+++ core/types.h
@@ -8,17 +8,17 @@
 namespace core
 {
 	using rawptr = void*;
-	using isize  = std::make_signed_t<std::size_t>;
-	using usize  = std::size_t;
+	using isize  = std::make_signed_t<size_t>;
+	using usize  = size_t;
 
-	using int8    = std::int8_t;
-	using uint8   = std::uint8_t;
-	using int16   = std::int16_t;
-	using uint16  = std::uint16_t;
-	using int32   = std::int32_t;
-	using uint32  = std::uint32_t;
-	using int64   = std::int64_t;
-	using uint64  = std::uint64_t;
+	using int8    = int8_t;
+	using uint8   = uint8_t;
+	using int16   = int16_t;
+	using uint16  = uint16_t;
+	using int32   = int32_t;
+	using uint32  = uint32_t;
+	using int64   = int64_t;
+	using uint64  = uint64_t;
 	using uint128 = __uint128_t;
 	using int128  = __int128_t;
 }
--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
-#include "array.h"
-#include <print>
-#include "types.h"
+#include <cstdint>
 
 int main()
 {
-	core::array_t<core::int32, static_cast<core::isize>(32)> my_array{};
-	for (core::int32 i = 0; i < my_array.cap(); i++)
-	{
-		my_array.append(i);
-	}
-	std::print("{0}\n", my_array[1]);
+	int32_t t;
 	return 0;
 }
