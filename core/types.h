16ba7cc27c5cff18cb2d76ffa829ef7a47807a84
Joshua Antikainen <joshua.antiakainen@gmail.com>
2026-09-18 23:06:32 -0600

    formatting

--- core/types.h
+++ core/types.h
@@ -5,18 +5,18 @@
 #include <cstddef>
 #include <cstdint>
 
-	using rawptr = void*;
-	using isize  = std::make_signed_t<size_t>;
-	using usize  = size_t;
+using rawptr = void*;
+using isize  = std::make_signed_t<size_t>;
+using usize  = size_t;
 
-	using int8    = int8_t;
-	using uint8   = uint8_t;
-	using int16   = int16_t;
-	using uint16  = uint16_t;
-	using int32   = int32_t;
-	using uint32  = uint32_t;
-	using int64   = int64_t;
-	using uint64  = uint64_t;
-	using uint128 = __uint128_t;
-	using int128  = __int128_t;
+using int8    = int8_t;
+using uint8   = uint8_t;
+using int16   = int16_t;
+using uint16  = uint16_t;
+using int32   = int32_t;
+using uint32  = uint32_t;
+using int64   = int64_t;
+using uint64  = uint64_t;
+using uint128 = __uint128_t;
+using int128  = __int128_t;
 #endif
